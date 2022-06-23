b0b920c469b395590041f654b1703cae6a2e4bcf
Josh Bailey <josh@vandervecken.com>
2022-06-23 23:30:52 +1200

    urls.

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 platform=$1
 fqbn=$2
+urls=https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json
 
 set -e
 sudo apt-get update
@@ -11,8 +12,8 @@ ln -s ~/arduino*/arduino ~/bin/arduino
 PATH=~/bin:$PATH curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=~/bin sh
 mkdir -p ~/.arduino15/packages
 PATH=~/bin:$PATH arduino-cli config init
-PATH=~/bin:$PATH arduino-cli core update-index --additional-urls https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json
-PATH=~/bin:$PATH arduino-cli core install $platform --additional-urls https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json
+PATH=~/bin:$PATH arduino-cli core update-index --additional-urls $urls
+PATH=~/bin:$PATH arduino-cli core install $platform --additional-urls $urls
 PATH=~/bin:$PATH arduino-cli lib install "MIDI Library"
 echo "void PIOC_Handler (void) __attribute__ ((weak));" >> `find ~/.arduino15/ -name WInterrupts.c`
 PATH=~/bin:$PATH arduino-cli compile --fqbn $fqbn vessel.ino
