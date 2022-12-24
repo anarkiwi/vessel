e52431a584ac7bcd07c85b836fb8f54316d35236
Josh Bailey <josh@vandervecken.com>
2022-12-24 16:55:03 +1300

    2.0.3.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ urls=https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_
 
 set -e
 sudo apt-get update && sudo apt-get install wget unzip
-wget -O/tmp/ide.zip https://downloads.arduino.cc/arduino-ide/arduino-ide_2.0.0_Linux_64bit.zip
+wget -O/tmp/ide.zip https://downloads.arduino.cc/arduino-ide/arduino-ide_2.0.3_Linux_64bit.zip
 unzip /tmp/ide.zip
 mkdir ~/bin
 ln -s ~/arduino*/arduino-ide ~/bin/arduino
